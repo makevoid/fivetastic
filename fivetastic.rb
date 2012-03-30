82754dbc7f12213435c21b765ec9a24e03616c3f
Francesco Canessa <makevoid@gmail.com>
2012-03-30 03:15:07 +0200

    simplified

--- fivetastic.rb
+++ fivetastic.rb
@@ -10,7 +10,9 @@ require "json" # load json gem?
 # use Rack::Reloader, 0
 # use Rack::Static, :urls => ["*"]
 
-PATHS = JSON.parse( File.read("#{PATH}/routes.json") )
+paths = JSON.parse( File.read("#{PATH}/routes.json") )
+paths.delete "/*"
+PATHS = paths
 
 MIME_TYPES = {
   ".haml"     => "text/haml",
@@ -22,24 +24,24 @@ MIME_TYPES = {
 Rack::Mime::MIME_TYPES.merge! MIME_TYPES
 
 class Fivetastic
-  
-  def self.call(env)    
+
+  def self.call(env)
     path = env["PATH_INFO"] || "/"
     file = if path == "/" || PATHS.keys.include?(path) || route_matches(path)
       "#{PATH}/index.html"
     else#if static_file? path
       "#{PATH}#{path}"
     end
-    
+
     unless File.exists? file
       [404, { "Content-Type" => get_type(:html)}, ["File '#{path}' not found - file: #{file}, paths: #{PATHS}"]]
     else
       render_file file
     end
   end
-  
+
   # private
-  
+
   def self.route_matches(path)
     match = nil
     PATHS.keys.each do |route|
@@ -47,28 +49,28 @@ class Fivetastic
         match = path.match(/#{route.gsub(/\*/, "(.+)").gsub(/\//, "\\/")}/)
         break if match
       end
-    end 
+    end
     match
   end
-  
+
   def self.get_type(type)
     Rack::Mime.mime_type ".#{type}"
   end
-  
+
   # def self.static_file?(path)
   #   path != "/" && File.exists?("#{PATH}/#{path}")
   # end
-  
+
   def self.expire_time(seconds)
     # (Time.now + seconds).strftime '%a, %d %b %Y %H:%M:%S GMT'
     CGI.rfc1123_date Time.now + seconds
   end
-  
+
   def self.render_file(file)
     puts "file: #{file}"
     body = File.read file
     cont_type = File.extname(file)[1..-1].to_sym
-    content_type = get_type cont_type 
+    content_type = get_type cont_type
     headers = { "Content-Type" => content_type }
     seconds = 62*60*60
     # headers.merge!( "Cache-Control" => "max-age=#{seconds}, public" )
@@ -76,5 +78,5 @@ class Fivetastic
     headers.merge!( "If-Modified-Since" =>  CGI.rfc1123_date(File.open(file).mtime) )
     [200, headers, [body]]
   end
-  
+
 end
